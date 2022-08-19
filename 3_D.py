d8759fbbc323cc937a91270ac80ea631c660e017
Kazuma Ishida <kazuma.ishida1618@gmail.com>
2022-08-19 23:47:48 +0900

    final commit

--- 3_D.py
+++ 3_D.py
@@ -1,15 +1,9 @@
-x = input("Enter the number :")
-y = x.split()
-list = [int(i) for i in y]
-a,b,c = list[0],list[1],list[2]
+x = list(map(int, input().split()))
+a, b, c = x[0], x[1], x[2]
 count = 0
 
 for i in range(a, b+1):
-    if c%i == 0:
+    if c % i == 0:
         count += 1
 
 print(count)
-
-    
-
-
