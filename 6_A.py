00b9ab64fa149dc3a49008a40b9d50b8e2a95bd9
Kazuma Ishida <kazuma.ishida1618@gmail.com>
2022-08-20 00:22:11 +0900

    pre_final

--- 6_A.py
+++ 6_A.py
@@ -1,17 +1,15 @@
 n = int(input())
 x = list(map(int, input().split()))
 
-if n%2 == 0:
-    m = int(n/2)
+if n % 2 == 0:
+    m = int(n / 2)
 else:
-    m = n//2
+    m = n // 2
 
 for i in range(m):
     a = x[i]
-    x[i] = x[-i-1]
-    x[-i-1] = a
+    x[i] = x[-i - 1]
+    x[-i - 1] = a
 
-L = " ".join(map(str,x))
+L = " ".join(map(str, x))
 print(L)
-
-
